fff4dbdb1d02d0870869d3ea107c1d127c9b6ba5
Pablo Bomfim <pablo.mb3107@gmail.com>
2023-08-26 17:08:32 -0300

    teste 02

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,17 +12,16 @@ terraform {
 
 }
 
-#S3
   backend "s3" {
     bucket         = "tf-terraform-hello-world-state-pablinhos"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
 
-  
-module "dynamodb_table" {
-  source   = "terraform-aws-modules/dynamodb-table/aws"
 
-  name     = "tf-terraform-hello-world-state-v1"
-  hash_key = "LockID"
+  module "dynamodb_table" {
+    source   = "terraform-aws-modules/dynamodb-table/aws"
+
+    name     = "tf-terraform-hello-world-state-v1"
+    hash_key = "LockID"
 }
