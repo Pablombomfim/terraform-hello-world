48ee2a0748adf7e1f30d7f772bb6fbd6c63c3454
Pablo Bomfim <pablo.mb3107@gmail.com>
2023-08-26 17:26:57 -0300

    teste 01

--- terraform/hello-world.tf
+++ terraform/hello-world.tf
@@ -104,9 +104,8 @@ resource "aws_instance" "hello-instance" {
 }
 
 #DynaboDb
-module "dynamodb_table" {
-  source   = "../../"
-  
+resource "aws_dynamodb_table" "this" {
+  source   = "terraform-aws-modules/dynamodb-table/aws"
   name     = "tf-terraform-hello-world-state-v1"
   hash_key = "LockID"
 }
